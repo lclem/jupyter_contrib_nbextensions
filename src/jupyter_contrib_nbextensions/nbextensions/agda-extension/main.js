2ea68500f63ce397f08ca23c1be515542b873e4a
Lorenzo Clemente <clementelorenzo@gmail.com>
2019-05-19 16:04:33 +0200

    improved error-handling

--- src/jupyter_contrib_nbextensions/nbextensions/agda-extension/main.js
+++ src/jupyter_contrib_nbextensions/nbextensions/agda-extension/main.js
@@ -240,7 +240,7 @@ definition: error1
                 cell.unrender();
             }
 
-            console.log("[agda-extension] handling error");
+            //console.log("[agda-extension] handling error");
 
             var fname = null;
             var re = /.*(?![\/])(.*\.agda)\:(\d+),\d+-(\d+)(,\d+)?/g;
@@ -248,7 +248,7 @@ definition: error1
 
             for (const match of matches) {
 
-                console.log("[agda-extension] found a match \"" + match + "\"");
+                //console.log("[agda-extension] found a match \"" + match + "\"");
 
                 fname = match[1];
                 var from = match[2];
